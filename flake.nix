4ca8ff3c9437499c8a7187413f3360cbdcc4d5b1
Benedikt Weyer <bw.development@pm.me>
2026-09-05 22:11:47 +0200

    feat: add Smithay compositor backends and input handling

--- flake.nix
+++ flake.nix
@@ -3,13 +3,41 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # smithay-drm-extras pins `libdisplay-info < 0.4.0`; nixos-unstable has 0.4.0, so
+    # pull the last compatible build (0.2.0) from an older channel for that one package.
+    nixpkgs-old.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-old, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              libdisplay-info = (import nixpkgs-old { inherit system; }).libdisplay-info;
+            })
+          ];
+        };
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+
+        buildInputs = with pkgs; [
+          wayland
+          wayland-protocols
+          libxkbcommon
+          libinput
+          seatd
+          udev
+          mesa
+          libglvnd
+          libgbm
+          libdisplay-info
+          vulkan-loader
+        ];
       in
       {
         devShells.default = pkgs.mkShell {
@@ -19,10 +47,10 @@
             rust-analyzer
             rustfmt
             clippy
-            pkg-config
-          ];
+          ] ++ nativeBuildInputs ++ buildInputs;
 
           RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
         };
       });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+static POSSIBLE_BACKENDS: &[&str] = &[
+    #[cfg(feature = "winit")]
+    "--winit : Run nested as a X11 or Wayland client using winit (for development).",
+    #[cfg(feature = "udev")]
+    "--tty-udev : Run standalone on a tty using DRM/KMS + libinput (requires root if without logind/seatd).",
+];
+
+#[allow(clippy::uninlined_format_args)]
 fn main() {
-    println!("Hello, world!");
+    if let Ok(env_filter) = tracing_subscriber::EnvFilter::try_from_default_env() {
+        tracing_subscriber::fmt()
+            .compact()
+            .with_env_filter(env_filter)
+            .init();
+    } else {
+        tracing_subscriber::fmt().compact().init();
+    }
+
+    profiling::register_thread!("Main Thread");
+
+    let arg = ::std::env::args().nth(1);
+    match arg.as_ref().map(|s| &s[..]) {
+        #[cfg(feature = "winit")]
+        Some("--winit") => {
+            tracing::info!("Starting ironland-copositor with winit backend");
+            ironland_copositor::winit::run_winit();
+        }
+        #[cfg(feature = "udev")]
+        Some("--tty-udev") => {
+            tracing::info!("Starting ironland-copositor on a tty using udev");
+            ironland_copositor::udev::run_udev();
+        }
+        Some(other) => {
+            tracing::error!("Unknown backend: {}", other);
+        }
+        None => {
+            #[allow(clippy::disallowed_macros)]
+            {
+                println!("USAGE: ironland-copositor --backend");
+                println!();
+                println!("Possible backends are:");
+                for b in POSSIBLE_BACKENDS {
+                    println!("\t{b}");
+                }
+            }
+        }
+    }
 }
