6436a0cc551b75bd814d91d89a8b5053225e514a
Benedikt Weyer <bw.development@pm.me>
2026-09-05 21:52:00 +0200

    chore: initialize Rust project with Nix development environment

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "ironland-copositor dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+            rustfmt
+            clippy
+            pkg-config
+          ];
+
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+      });
+}
